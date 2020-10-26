11df621bfb76acd05e870e4219be5563d685e2d7
Victor <dblvix@gmail.com>
2020-10-26 22:06:03 +0300

    database connected

--- project_management/settings.py
+++ project_management/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'project_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project-management',
+        'USER': 'root',
+        'PASSWORD': 'wasd',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
