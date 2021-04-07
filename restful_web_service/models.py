9d3595c4b243839ffec33df1d30e24fa4322f260
FilippovichWerk <56321464+risk03@users.noreply.github.com>
2021-04-07 10:04:13 +0300

    model added

--- restful_web_service/models.py
+++ restful_web_service/models.py
@@ -13,17 +13,119 @@ class TaskComponent(models.Model):
 
 
 class TaskLeaf(TaskComponent):
-    start = models.DateTimeField(null=True)
-    end = models.DateTimeField(null=True)
+    start = models.DateTimeField(null=True, blank=True)
+    end = models.DateTimeField(null=True, blank=True)
     STATUS = (
     ('NEW', 'New'), ('PRO', 'In progress'), ('COM', 'Completed'), ('REJ', 'Rejected'), ('REV', 'Send to revision'))
     status = models.CharField(max_length=3, choices=STATUS)
-    system = models.ForeignKey("SystemComponent", null=True, related_name='task', on_delete=models.SET_NULL)
+    system = models.ForeignKey("SystemComponent", null=True, blank=True, related_name='task', on_delete=models.SET_NULL)
+    prev = models.ManyToManyField('self', related_name="next", symmetrical=False, blank=True)
+    duration = models.DurationField()
+    real_early_start = models.DateTimeField(null=True, blank=True)
+    real_late_start = models.DateTimeField(null=True, blank=True)
+    real_early_end = models.DateTimeField(null=True, blank=True)
+    real_late_end = models.DateTimeField(null=True, blank=True)
+    prop_early_start = models.DateTimeField(null=True, blank=True)
+    prop_late_start = models.DateTimeField(null=True, blank=True)
+    prop_early_end = models.DateTimeField(null=True, blank=True)
+    prop_late_end = models.DateTimeField(null=True, blank=True)
+
+    @property
+    def tei(self):
+        if self.prev:
+            return max(x.early_end for x in self.prev)
+        else:
+            return self.early_start
+    
+    @property
+    def tli(self):
+        if self.prev:
+            return max(x.late_end for x in self.prev)
+        else:
+            return self.early_start
+
+    @property
+    def tej(self):
+        if self.next:
+            return min(x.early_start for x in self.next)
+        else:
+            return self.late_end
+
+    @property
+    def tlj(self):
+        if self.next:
+            return min(x.late_start for x in self.next)
+        else:
+            return self.late_end
+
+    @property
+    def event_reserve(self):
+        return self.tli - self.tei
+
+    @property
+    def full_reserve(self):
+        return self.tlj - self.tei - self.duration
+
+    @property
+    def free_reserve(self):
+        return self.tej - self.tei - self.duration
+
+    @property
+    def is_critical(self) -> bool:
+        return self.late_start == self.early_start
+
+    @property
+    def early_start(self):
+        if self.start:
+            return self.start
+        l = [x for x in [self.real_early_start, self.prop_early_start] if x is not None]
+        return max(l) if l else None
+
+
+    @early_start.setter
+    def early_start(self, value):
+        self.prop_early_start = value
+
+    @property
+    def late_start(self):
+        if self.start:
+            return self.start
+        l = [x for x in [self.real_late_start, self.prop_late_start] if x is not None]
+        return min(l) if l else None
+
+
+    @late_start.setter
+    def late_start(self, value):
+        self.prop_late_start = value
+
+    @property
+    def early_end(self):
+        if self.end:
+            return self.end
+        l = [x for x in [self.real_early_end, self.prop_early_end] if x is not None]
+        return max(l) if l else None
+
+
+    @early_end.setter
+    def early_end(self, value):
+        self.prop_early_end = value
+
+    @property
+    def late_end(self):
+        if self.end:
+            return self.end
+        l = [x for x in [self.real_late_end, self.prop_late_end] if x is not None]
+        return min(l) if l else None
+
+
+    @late_end.setter
+    def late_end(self, value):
+        self.prop_late_end = value
 
 
 class Artefact(models.Model):
     title = models.CharField(max_length=255, null=False)
-    description = models.CharField(max_length=1024, null=True)
+    description = models.CharField(max_length=1024, null=True, blank=True)
     task = models.ForeignKey("TaskLeaf", null=False, on_delete=models.CASCADE, related_name="artefact")
 
     def __str__(self):
@@ -47,15 +149,15 @@ class Division(StructureComponent):
 
 class Employee(StructureComponent):
     full_name = models.CharField(max_length=255, null=False)
-    short_name = models.CharField(max_length=255, null=True)
+    short_name = models.CharField(max_length=255, null=True, blank=True)
     position = models.ForeignKey("Position", null=True, blank=True, on_delete=models.SET_NULL)
-    login = models.CharField(max_length=32, null=True, unique=True)
-    salt = models.CharField(max_length=32, null=True)
-    hash = models.CharField(max_length=32, null=True)
+    login = models.CharField(max_length=32, null=True, unique=True, blank=True)
+    salt = models.CharField(max_length=32, null=True, blank=True)
+    hash = models.CharField(max_length=32, null=True, blank=True)
     isadmin = models.BooleanField(null=False, default=False)
 
     def __str__(self):
-        return (self.short_name if self.short_name else self.full_name) + ', ' + self.position.name
+        return (self.short_name if self.short_name else self.full_name) + (', ' + self.position.name if self.position else '')
 
 
 class Position(models.Model):
