49fa7e14c5a49d6c6c23438fc6680387d2c7af10
FilippovichWerk <56321464+risk03@users.noreply.github.com>
2021-04-08 09:59:14 +0300

    new_version

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!E:\python\project_management\venv\Scripts\python.exe
+#!c:\users\stud\source\repos\project-management\venv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
